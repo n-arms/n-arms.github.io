66a6c32139d490bf837af0b76e28989fb8cde20b
n-arms <75282394+n-arms@users.noreply.github.com>
2024-09-06 11:22:11 -0400

    Change fonts to Source Code Pro

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <svelte:head>
 	<title>Nick Armstrong</title>
+	<link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro' rel='stylesheet' type='text/css'>
 </svelte:head>
 
 <div id="header">
@@ -19,7 +20,10 @@
 	Waterloo Computer Engineering '29
 </p>
 <p>
-	I'm a comp eng undergrad who is passionate about compilers, robotics pathfinding and trajectory optimization, gpu programming, and embedded development.
+	I'm a comp eng undergrad who is passionate about compilers, robotics pathfinding and trajectory optimization, GPU programming, and embedded development.
+</p>
+<p>
+	I'm currently seeking employment for two different 4 month co-op jobs, one starting January 2025, and the next starting September 2025.
 </p>
 
 <style>
@@ -37,5 +41,8 @@
 		flex-direction: row;
 		align-items: baseline;
 		justify-content: space-between;
+	}
+	h1, p {
+		font-family: 'Source Sans Pro', Arial, Helvetica, sans-serif;
 	}
 </style>
